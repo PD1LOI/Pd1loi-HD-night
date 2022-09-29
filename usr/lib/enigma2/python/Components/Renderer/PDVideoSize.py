795432c28346908f960d6fb7d6cf195ffb68d3df
L Kuperij <l.kuperij@gmail.com>
2022-09-29 10:39:23 +0200

    WeatherPlugin
    Weatherplugin vewijderd plus componenten.

---
+++ usr/lib/enigma2/python/Components/Renderer/PDVideoSize.py
@@ -0,0 +1,28 @@
+# Embedded file name: /usr/lib/enigma2/python/Components/Renderer/PDVideoSize.py
+from Components.VariableText import VariableText
+from enigma import eLabel, iServiceInformation
+from Renderer import Renderer
+
+class PDVideoSize(Renderer, VariableText):
+
+    def __init__(self):
+        Renderer.__init__(self)
+        VariableText.__init__(self)
+
+    GUI_WIDGET = eLabel
+
+    def changed(self, what):
+        service = self.source.service
+        info = service and service.info()
+        if info is None:
+            self.text = ''
+            return
+        else:
+            xresol = info.getInfo(iServiceInformation.sVideoWidth)
+            yresol = info.getInfo(iServiceInformation.sVideoHeight)
+            if xresol > 0:
+                self.text = str(xresol) + '  ' + str(yresol)
+            else:
+                self.text = ''
+            return
+            return
